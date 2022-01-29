1d03c2df8103b2e542ac8af735985da39ff0aa54
Varun Warrier <varunswarrier@gmail.com>
2022-01-29 16:30:40 -0500

    added commands for intake, neck, shooter

---
+++ src/main/java/frc/robot/commands/intake/ToggleIntakeCommand.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.IntakeSubsystem.IntakeStatus;
+
+public class ToggleIntakeCommand extends CommandBase {
+  private final IntakeSubsystem intakeSubsystem;
+  /** Creates a new IntakeUpCommand. */
+  public ToggleIntakeCommand(IntakeSubsystem intakeSubsystem) {
+    this.intakeSubsystem = intakeSubsystem;
+    addRequirements(intakeSubsystem);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (intakeSubsystem.getIntakePostion() == IntakeStatus.DOWN) {
+      intakeSubsystem.retract();
+    } 
+    else {
+      intakeSubsystem.extend();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
