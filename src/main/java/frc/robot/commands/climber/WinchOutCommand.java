5759d212dc5abcabe85d563ad4a915f784dcd02d
Varun Warrier <varunswarrier@gmail.com>
2022-03-02 18:56:37 -0500

    winch cod

---
+++ src/main/java/frc/robot/commands/climber/WinchOutCommand.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.climber;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.ClimberConstants;
+import frc.robot.subsystems.ClimberSubsystem;
+import frc.robot.subsystems.ClimberSubsystem.MovingBrakeStatus;
+
+public class WinchOutCommand extends CommandBase {
+  
+  private ClimberSubsystem climberSubsystem;
+
+  public WinchOutCommand(ClimberSubsystem climberSubsystem) {
+    this.climberSubsystem = climberSubsystem;
+    addRequirements(climberSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (climberSubsystem.getBrakeStatus() == MovingBrakeStatus.OFF) {
+      climberSubsystem.moveWinch(-ClimberConstants.CLIMBER_WINCH_SPEED);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
