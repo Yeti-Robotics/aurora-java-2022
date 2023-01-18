051054e7425d7cd3e1e6fdf8fc14a5410cd6a151
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2023-01-17 19:37:21 -0500

    finally ready for 2023 code

--- src/main/java/frc/robot/utils/AutoBuilder.java
+++ src/main/java/frc/robot/utils/AutoBuilder.java
@@ -12,19 +12,13 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.TurretConstants;
 import frc.robot.Robot.AutoModes;
 import frc.robot.RobotContainer;
 import frc.robot.commands.LED.ShooterLEDCommand;
 import frc.robot.commands.commandgroups.AllInCommand;
-import frc.robot.commands.commandgroups.AllInCommandGroup;
 import frc.robot.commands.commandgroups.AllOutCommand;
 import frc.robot.commands.drivetrain.DriveForDistanceCommand;
 import frc.robot.commands.drivetrain.TurnToTargetDriveCommand;
@@ -43,6 +37,19 @@ public class AutoBuilder {
   private ParallelCommandGroup command;
   private SequentialCommandGroup subsystemCommandGroup;
   private SequentialCommandGroup pathCommandGroup;
+
+  private final CommandBase allInCommand = Commands.sequence(new InstantCommand(() -> {
+    robotContainer.intakeSubsystem.stopRoll();
+    robotContainer.neckSubsystem.stopNeck();
+  }), new ConditionalCommand(new RunCommand(() -> {
+    robotContainer.neckSubsystem.moveUp(0.8);
+  }).andThen(new ConditionalCommand(new WaitCommand(0.25), new InstantCommand(), () -> !robotContainer.neckSubsystem.getUpperBeamBreak())), new RunCommand(() -> {
+    robotContainer.intakeSubsystem.rollIn();
+    if (robotContainer.neckSubsystem.getLowerBeamBreak()) robotContainer.neckSubsystem.moveUp(0.3);
+  }), () -> ShooterSubsystem.atSetPoint), new InstantCommand(() -> {
+    robotContainer.intakeSubsystem.stopRoll();
+    robotContainer.neckSubsystem.stopNeck();
+  }));
 
   private void twoBallAuto() {
     subsystemCommandGroup.addCommands(
@@ -242,8 +249,7 @@ public class AutoBuilder {
   private void threeBall() {
     subsystemCommandGroup.addCommands(
         new ToggleIntakeCommand(robotContainer.intakeSubsystem),
-        new AllInCommandGroup(robotContainer.intakeSubsystem, robotContainer.neckSubsystem)
-            .withTimeout(2.0),
+        allInCommand.withTimeout(2.0),
         new ToggleIntakeCommand(robotContainer.intakeSubsystem),
         new InstantCommand(() -> ShooterSubsystem.setPoint = 3800.0),
         new ToggleFlywheelHighCommand(shooterLEDCommand),
@@ -256,8 +262,7 @@ public class AutoBuilder {
         new ToggleFlywheelHighCommand(shooterLEDCommand),
         new WaitCommand(1.0),
         new ToggleIntakeCommand(robotContainer.intakeSubsystem),
-        new AllInCommandGroup(robotContainer.intakeSubsystem, robotContainer.neckSubsystem)
-            .withTimeout(3.0),
+        allInCommand.withTimeout(3.0),
         new InstantCommand(() -> ShooterSubsystem.setPoint = 3800.0),
         new ToggleFlywheelHighCommand(shooterLEDCommand),
         new WaitCommand(1.25),
